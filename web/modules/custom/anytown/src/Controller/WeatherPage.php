57a71fa832353f4ec45ecb4e98afdb83e6d8edcd
sjmaitland <31549445+sjmaitland@users.noreply.github.com>
2025-03-04 17:05:18 -0500

    Added weather json file and updated weatherpage with new controller code to pull the json.

--- web/modules/custom/anytown/src/Controller/WeatherPage.php
+++ web/modules/custom/anytown/src/Controller/WeatherPage.php
@@ -1,36 +1,88 @@
 <?php
-    declare(strict_types=1);
 
-    namespace Drupal\anytown\Controller;
+declare(strict_types=1);
 
-    use Drupal\Core\Controller\ControllerBase;
+namespace Drupal\anytown\Controller;
 
-    /**
-     *  Controller for anytown.weather_page route
-     */
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\Logger\RfcLogLevel;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 
-     class WeatherPage extends ControllerBase {
-        /**
-         * Builds the response
-         */
+/**
+ * Controller for anytown.weather_page route.
+ */
+class WeatherPage extends ControllerBase {
 
-         public function build(string $style): array {
-            // Style should be one of 'short' or 'extended'. And default to 'short'.
-            $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
+  use AutowireTrait;
 
-            $build['content'] = [
-                '#type' => 'markup',
-                '#markup' => '<p>The weather forecast for this week is sunny with a chance of meatballs.</p>',
-            ];
+  /**
+   * HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  private $httpClient;
 
-            if ($style === 'extended') {
-                $build['content_extended'] = [
-                    '#type' => 'markup',
-                    '#markup' => '<p><strong>Extended forecast:</strong> Looking ahead to next week, we expect some snow.</p>',
-                ];
-            }
+  /**
+   * Logging service, set to 'anytown' channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  private $logger;
 
-            return $build;
-         }
-     }
-?>
+  /**
+   * WeatherPage controller constructor.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   HTTP client.
+   */
+  public function __construct(ClientInterface $http_client) {
+    $this->httpClient = $http_client;
+    $this->logger = $this->getLogger('anytown');
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function build(string $style): array {
+    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
+
+    $url = 'https://module-developer-guide-demo-site.ddev.site/modules/custom/anytown/data/weather_forecast.json';
+    $data = NULL;
+
+    try {
+      $response = $this->httpClient->get($url);
+      $data = json_decode($response->getBody()->getContents());
+    }
+    catch (RequestException $e) {
+      $this->logger->log(RfcLogLevel::WARNING, $e->getMessage());
+    }
+
+    if ($data) {
+      $forecast = '<ul>';
+      foreach ($data->list as $day) {
+        $weekday = ucfirst($day->day);
+        $description = array_shift($day->weather)->description;
+        // Convert units in Kelvin to Fahrenheit.
+        $high = round(($day->main->temp_max - 273.15) * 9 / 5 + 32);
+        $low = round(($day->main->temp_min - 273.15) * 9 / 5 + 32);
+        $forecast .= "<li>$weekday will be <em>$description</em> with a high of $high and a low of $low.</li>";
+      }
+      $forecast .= '</ul>';
+    }
+    else {
+      $forecast = '<p>Could not get the weather forecast. Dress for anything.</p>';
+    }
+
+    $output = "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>";
+    $output .= $forecast;
+
+    return [
+      '#markup' => $output,
+    ];
+    
+  }
+
+}
