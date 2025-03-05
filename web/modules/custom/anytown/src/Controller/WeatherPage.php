8438ce01e00e302f7df6e29486ae3b3e1ed963c0
sjmaitland <31549445+sjmaitland@users.noreply.github.com>
2025-03-05 16:37:52 -0500

    Added new files.

--- web/modules/custom/anytown/src/Controller/WeatherPage.php
+++ web/modules/custom/anytown/src/Controller/WeatherPage.php
@@ -4,70 +4,59 @@ declare(strict_types=1);
 
 namespace Drupal\anytown\Controller;
 
+use Drupal\anytown\ForecastClientInterface;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\DependencyInjection\AutowireTrait;
-use Drupal\Core\Logger\RfcLogLevel;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Controller for anytown.weather_page route.
+ * Controller for anytown.weather_page rout.
  */
 class WeatherPage extends ControllerBase {
-
-  use AutowireTrait;
-
+  
   /**
-   * HTTP client.
-   *
-   * @var \GuzzleHttp\ClientInterface
+   * Forecast API client.
+   * 
+   * @var Drupal\anytown\ForecastClientInterface
    */
-  private $httpClient;
-
-  /**
-   * Logging service, set to 'anytown' channel.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  private $logger;
+  private $forecastClient;
 
   /**
    * WeatherPage controller constructor.
-   *
-   * @param \GuzzleHttp\ClientInterface $http_client
-   *   HTTP client.
+   * 
+   * @param \Drupal\anytown\ForecastClientInterface $forecast_client
+   *  Forecast API client service.
    */
-  public function __construct(ClientInterface $http_client) {
-    $this->httpClient = $http_client;
-    $this->logger = $this->getLogger('anytown');
+  public function __construct(ForecastClientInterface $forecast_client) {
+    $this->forecastClient = $forecast_client;
+  }
+
+  /**
+   *  {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('anytown.forecast_client')
+    );
   }
 
   /**
    * Builds the response.
    */
   public function build(string $style): array {
-    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    // Style should be one of 'short', or 'extended'. 'Short' is the default.
     $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
 
     $url = 'https://module-developer-guide-demo-site.ddev.site/modules/custom/anytown/data/weather_forecast.json';
-    $data = NULL;
-
-    try {
-      $response = $this->httpClient->get($url);
-      $data = json_decode($response->getBody()->getContents());
-    }
-    catch (RequestException $e) {
-      $this->logger->log(RfcLogLevel::WARNING, $e->getMessage());
-    }
-
-    if ($data) {
+    $forecast_data = $this->forecastClient->getForecastData($url);
+    if ($forecast_data) {
       $forecast = '<ul>';
-      foreach ($data->list as $day) {
-        $weekday = ucfirst($day->day);
-        $description = array_shift($day->weather)->description;
-        // Convert units in Kelvin to Fahrenheit.
-        $high = round(($day->main->temp_max - 273.15) * 9 / 5 + 32);
-        $low = round(($day->main->temp_min - 273.15) * 9 / 5 + 32);
+      foreach ($forecast_data as $item) {
+        [
+          'weekday' => $weekday,
+          'description' => $description,
+          'high' => $high,
+          'low' => $low,
+        ] = $item;
         $forecast .= "<li>$weekday will be <em>$description</em> with a high of $high and a low of $low.</li>";
       }
       $forecast .= '</ul>';
@@ -78,11 +67,11 @@ class WeatherPage extends ControllerBase {
 
     $output = "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>";
     $output .= $forecast;
+    $output .= '<h3>Weather related closures</h3><ul><li>Ice rink closed until winter - please stay off while we prepare it.</li><li>Parking behind Apple Lane is still closed from all the rain last week.</li></ul>';
 
     return [
       '#markup' => $output,
     ];
-    
   }
 
 }
